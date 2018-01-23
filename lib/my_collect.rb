ef88833530d9487e42bda7f7ec42d900b5aacb25
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-23 20:48:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array)
    i = 0
   while i < array.size
-    block_given? # optional
-    yield(array[i]) # if block, then yield to block # also need to pass argument(s) to yield, otherwise block is operating on nil
+    block_given?
+    yield(array[i]) 
     i += 1
   end
-array
+array.upcase
 end
